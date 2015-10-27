8012e8a1222baf88a731d854ef352b1910b0f5a3
Nuno Mota <hypernuno@hotmail.com>
2015-10-27 15:24:07 +0100

    Creates class constructor and initializes class' private fields

--- HikingApp/app/src/main/java/GPSTracker/GPSTracker.java
+++ HikingApp/app/src/main/java/GPSTracker/GPSTracker.java
@@ -1,13 +1,26 @@
 package GPSTracker;
 
+import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 public class GPSTracker implements LocationListener {
+
+    private Context activityContext = null;
+    private LocationManager locationManager = null;
+    private LocationListener locationListener = null;
+
+    public GPSTracker(Context activityContext) {
+        this.activityContext = activityContext;
+        locationManager = (LocationManager) activityContext.getSystemService(Context.LOCATION_SERVICE);
+        
+    }
+
     @Override
     public void onLocationChanged(Location location) {
-        
+
     }
 
     @Override
