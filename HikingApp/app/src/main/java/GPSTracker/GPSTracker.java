cf5ba9558b286eab0c2a0a5a162745fd6232681a
Nuno Mota <hypernuno@hotmail.com>
2015-10-27 16:41:52 +0100

    Adds javadoc & commenting

--- HikingApp/app/src/main/java/GPSTracker/GPSTracker.java
+++ HikingApp/app/src/main/java/GPSTracker/GPSTracker.java
@@ -11,20 +11,42 @@ import com.google.android.gms.maps.model.LatLng;
 import GPSTracker.Exceptions.GPSServiceNotAvailable;
 import GPSTracker.Exceptions.NoPositionTrackedException;
 
+/**
+ * Class used to fetch device's GPS-related information
+ * (such has latitude, longitude and altitude)
+ */
 public class GPSTracker implements LocationListener {
 
     private Context activityContext = null;
     private LocationManager locationManager = null;
 
+    //updated automatically when user's location changes
     private Location currentLocation = null;
 
+    /**
+     * Class' constructor
+     * @param activityContext the context (or activity) from which the GPSTracker was instantiated from
+     * @throws GPSServiceNotAvailable exception thrown if GPS service is not available
+     */
     public GPSTracker(Context activityContext) throws GPSServiceNotAvailable {
         this.activityContext = activityContext;
         locationManager = (LocationManager) activityContext.getSystemService(Context.LOCATION_SERVICE);
         if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) throw new GPSServiceNotAvailable();
     }
 
+    /**
+     * Method used to get user's location
+     * @return new LatLng object containing information about the device's current position
+     * @throws NoPositionTrackedException exception thrown when there is no position tracked yet
+     */
     public LatLng getLatLng() throws NoPositionTrackedException {
+
+        /* This check is redundant, because an instance of
+         * this class cannot be created if there is no GPS service.
+         * Knowing there is GPS service, when this method is called,
+         * means that onLocationChange method was called at least
+         * once - so there should be a location already stored.
+         */
         if (currentLocation == null) {
             throw new NoPositionTrackedException();
         }
@@ -38,6 +60,10 @@ public class GPSTracker implements LocationListener {
         }
         return currentLocation.toString();
     }
+
+    /*///////////////////////////////////////////
+    ///////// Interface's default methods ///////
+    ///////////////////////////////////////////*/
 
     @Override
     public void onLocationChanged(Location location) {
