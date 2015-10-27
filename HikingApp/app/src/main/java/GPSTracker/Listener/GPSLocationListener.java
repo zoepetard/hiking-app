a4e64b001508de46142545555bcfed2d99e4ed24
Nuno Mota <hypernuno@hotmail.com>
2015-10-27 17:46:32 +0100

    Creates standalone Listener for modularity

---
+++ HikingApp/app/src/main/java/GPSTracker/Listener/GPSLocationListener.java
@@ -0,0 +1,29 @@
+package GPSTracker.Listener;
+
+import android.location.Location;
+import android.os.Bundle;
+
+import com.google.android.gms.location.LocationListener;
+
+public class GPSLocationListener implements LocationListener {
+
+    //updated automatically when user's location changes
+    private Location currentLocation = null;
+
+    public GPSLocationListener(Location initialLocation) {
+        currentLocation = initialLocation;
+    }
+
+    public Location getCurLocation() {
+        return this.currentLocation;
+    }
+
+    /*///////////////////////////////////////////
+    ///////// Interface's default methods ///////
+    ///////////////////////////////////////////*/
+
+    @Override
+    public void onLocationChanged(Location location) {
+        this.currentLocation = location;
+    }
+}
