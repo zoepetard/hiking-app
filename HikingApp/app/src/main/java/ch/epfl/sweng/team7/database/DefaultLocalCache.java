a46f7f9519d9a29ff30e273ef52f953d4d547fa8
Simon Schuetz <simon.schuetz@epfl.ch>
2015-11-05 22:28:18 +0100

    removes debug code from DefaultLocalCache

--- HikingApp/app/src/main/java/ch/epfl/sweng/team7/database/DefaultLocalCache.java
+++ HikingApp/app/src/main/java/ch/epfl/sweng/team7/database/DefaultLocalCache.java
@@ -5,14 +5,9 @@
  */
 package ch.epfl.sweng.team7.database;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
-
-import ch.epfl.sweng.team7.network.RawHikeData;
 
 
 class DefaultLocalCache implements LocalCache {
@@ -20,29 +15,6 @@ class DefaultLocalCache implements LocalCache {
     private final HashMap<Long,HikeData> mHikesCache = new FixedSizeHashMap<>(HIKES_CACHE_MAX_SIZE);
 
     public DefaultLocalCache()  {
-        if(false) {
-            final String PROPER_JSON_ONEHIKE = "{\n"
-                    + "  \"hike_id\": 1,\n"
-                    + "  \"owner_id\": 48,\n"
-                    + "  \"date\": 123201,\n"
-                    + "  \"hike_data\": [\n"
-                    + "    [0.0, 0.0, 123201],\n"
-                    + "    [0.1, 0.1, 123202],\n"
-                    + "    [0.2, 0.0, 123203],\n"
-                    + "    [0.3,89.9, 123204],\n"
-                    + "    [0.4, 0.0, 123205]\n"
-                    + "  ]\n"
-                    + "}\n";
-            try {
-                Thread.sleep(10); // this code should fail execution when called from UI thread
-            } catch(InterruptedException e) {
-                // pass
-            }
-            try {
-                mHikesCache.put(new Long(1), new DefaultHikeData(RawHikeData.parseFromJSON(new JSONObject(PROPER_JSON_ONEHIKE))));
-            } catch(JSONException e) {
-            }
-        }
     }
 
     public boolean hasHike(long hikeId) {
