c02cb64702baeffdf345545685533e877f65a67b
Nuno Mota <hypernuno@hotmail.com>
2015-11-06 20:36:10 +0100

    Cleans GSPTracker package in order to use GoogleMaps built-in location

--- HikingApp/app/src/main/java/ch/epfl/sweng/team7/gpsTracker/GPSTracker.java
+++ HikingApp/app/src/main/java/ch/epfl/sweng/team7/gpsTracker/GPSTracker.java
@@ -6,67 +6,27 @@ import android.location.LocationManager;
 
 import com.google.android.gms.maps.model.LatLng;
 
-import ch.epfl.sweng.team7.gpsTracker.exceptions.GPSServiceNotAvailableException;
-import ch.epfl.sweng.team7.gpsTracker.exceptions.NoPositionTrackedException;
-import ch.epfl.sweng.team7.gpsTracker.listener.GPSLocationListener;
-
 /**
  * Class used to fetch device's GPS-related information
  * (such has latitude, longitude and altitude)
  */
 public class GPSTracker {
 
-    private static final long UPDATE_MIN_TIME_INTERVAL = 5000L;
-    private static final float UPDATE_MIN_DISTANCE = 15.0f;
+    private Location currentLocation = null;
 
-    private Context activityContext = null;
-    private LocationManager locationManager = null;
-    private GPSLocationListener locationListener = null;
+    public GPSTracker() {
 
-    /**
-     * Class' constructor
-     * @param activityContext the context (or activity) from which the GPSTracker was instantiated from
-     * @throws GPSServiceNotAvailableException exception thrown if GPS service is not available
-     */
-    public GPSTracker(Context activityContext) throws GPSServiceNotAvailableException {
-        this.activityContext = activityContext;
-        locationManager = (LocationManager) activityContext.getSystemService(Context.LOCATION_SERVICE);
-        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
-            throw new GPSServiceNotAvailableException();
-
-        try {
-            locationListener = new GPSLocationListener(locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER));
-        } catch (SecurityException e) {
-            //TODO display a warning saying that the GPS service cannot be accessed
-        }
-        //TODO call requestLocationUpdates to set up automatic position updating
     }
 
-    /**
-     * Method used to get user's location
-     * @return new LatLng object containing information about the device's current position
-     * @throws NoPositionTrackedException exception thrown when there is no position tracked yet
-     */
-    public LatLng getLatLng() throws NoPositionTrackedException {
+    public LatLng getLatLng() {
 
-        Location currentLocation = fetchCurrentLocation();
-
-        if (currentLocation == null) {
-            throw new NoPositionTrackedException();
-        }
-        return new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
     }
 
     @Override
     public String toString() {
-        Location currentLocation = fetchCurrentLocation();
         if (currentLocation == null) {
             return "No position tracked yet";
         }
         return currentLocation.toString();
-    }
-
-    private Location fetchCurrentLocation() {
-        return (locationListener != null)?locationListener.getCurLocation():null;
     }
 }
