614677fef6925155463d1cdccbae1dcc35d9f831
Nuno Mota <hypernuno@hotmail.com>
2015-11-07 16:01:13 +0100

    Basic imports for unit testing

--- HikingApp/app/src/androidTest/java/ch/epfl/sweng/team7/gpsTracker/containers/coordinates/GeoCoordsTest.java
+++ HikingApp/app/src/androidTest/java/ch/epfl/sweng/team7/gpsTracker/containers/coordinates/GeoCoordsTest.java
@@ -1,4 +1,17 @@
 package ch.epfl.sweng.team7.gpsTracker.containers.coordinates;
 
+import static org.junit.Assert.*;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import ch.epfl.sweng.team7.gpsTracker.containers.coordinates.GeoCoords;
+
+@RunWith(AndroidJUnit4.class)
 public class GeoCoordsTest {
+    
 }
