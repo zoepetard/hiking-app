2b01e02f27f5cfc0d363e1454e71f64a45d2e323
Nuno Mota <hypernuno@hotmail.com>
2015-11-07 16:14:10 +0100

    Removes unused imports

--- HikingApp/app/src/androidTest/java/ch/epfl/sweng/team7/gpsTracker/GPSTrackerTest.java
+++ HikingApp/app/src/androidTest/java/ch/epfl/sweng/team7/gpsTracker/GPSTrackerTest.java
@@ -1,15 +1,10 @@
 package ch.epfl.sweng.team7.gpsTracker;
 
-import static org.junit.Assert.*;
 import android.support.test.runner.AndroidJUnit4;
-
-import com.google.android.gms.maps.model.LatLng;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import ch.epfl.sweng.team7.gpsTracker.containers.coordinates.GeoCoords;
 
 @RunWith(AndroidJUnit4.class)
 public class GPSTrackerTest {
