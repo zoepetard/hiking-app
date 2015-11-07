16846d77c268f56900b34f3a560641bb6e3be4b6
Nuno Mota <hypernuno@hotmail.com>
2015-11-07 21:16:37 +0100

    Adds simple unit Tests to GPSPath

--- HikingApp/app/src/androidTest/java/ch/epfl/sweng/team7/gpsService/containers/GPSPathTest.java
+++ HikingApp/app/src/androidTest/java/ch/epfl/sweng/team7/gpsService/containers/GPSPathTest.java
@@ -1,11 +1,30 @@
 package ch.epfl.sweng.team7.gpsService.containers;
 
+import static org.junit.Assert.*;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import ch.epfl.sweng.team7.gpsService.containers.coordinates.GeoCoords;
+
 @RunWith(AndroidJUnit4.class)
 public class GPSPathTest {
+
+    private GPSPath gpsPath;
+
+    @Test
+    public void testSizeOnEmptyAndAfterAddPath() {
+        gpsPath = new GPSPath();
+        assertEquals(0, gpsPath.getFootPrintCount());
+        gpsPath.addFootPrint(new GPSFootPrint(new GeoCoords(0, 0, 0), 0));
+        assertEquals(1, gpsPath.getFootPrintCount());
+    }
+
+    @Test
+    public void testSizeAfterNullAdd() {
+        gpsPath = new GPSPath();
+        gpsPath.addFootPrint(null);
+        assertEquals(0, gpsPath.getFootPrintCount());
+    }
 }
