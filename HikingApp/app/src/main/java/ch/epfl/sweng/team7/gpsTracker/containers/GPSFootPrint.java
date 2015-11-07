0233d76ecb4c89ffac9d8c802d5175bf6d9bbf90
Nuno Mota <hypernuno@hotmail.com>
2015-11-07 15:36:49 +0100

    Adds simple Null check and toString method to GPSFootPrint

--- HikingApp/app/src/main/java/ch/epfl/sweng/team7/gpsTracker/containers/GPSFootPrint.java
+++ HikingApp/app/src/main/java/ch/epfl/sweng/team7/gpsTracker/containers/GPSFootPrint.java
@@ -2,13 +2,23 @@ package ch.epfl.sweng.team7.gpsTracker.containers;
 
 import ch.epfl.sweng.team7.gpsTracker.containers.coordinates.GeoCoords;
 
+/**
+ * Class that contains all the information needed to
+ * store every time we read the gps state.
+ */
 public class GPSFootPrint {
 
-    private GeoCoords geoCoords;
-    private long timeStamp;
+    private GeoCoords geoCoords;    //latitude, longitude and altitude
+    private long timeStamp;         //time in milliseconds since January 1, 1970
 
-    public GPSFootPrint(GeoCoords geoCoords, long timeStamp) {
+    public GPSFootPrint(GeoCoords geoCoords, long timeStamp) throws NullPointerException {
+        if (geoCoords == null) throw new NullPointerException("Cannot create footprint from null coordinates");
         this.geoCoords = geoCoords;
         this.timeStamp = timeStamp;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[Coords: %s | Time: %d]", geoCoords.toString(), timeStamp);
     }
 }
