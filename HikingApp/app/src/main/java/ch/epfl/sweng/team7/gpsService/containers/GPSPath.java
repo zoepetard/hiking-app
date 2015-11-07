21c803be671a77aff0d983c58778616b1c44c7dd
Nuno Mota <hypernuno@hotmail.com>
2015-11-07 20:13:13 +0100

    Creates GPSPath to store all hike-related information

---
+++ HikingApp/app/src/main/java/ch/epfl/sweng/team7/gpsService/containers/GPSPath.java
@@ -0,0 +1,13 @@
+package ch.epfl.sweng.team7.gpsService.containers;
+
+/**
+ * Class to be used as a container of GPSFootPrints.
+ * When we finish populating it, it should have all
+ * the information needed to reproduce an entire hike.
+ */
+public final class GPSPath {
+
+    public GPSPath() {
+
+    }
+}
