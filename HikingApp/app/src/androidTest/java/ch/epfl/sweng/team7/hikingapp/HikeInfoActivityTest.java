7534cc13e242bdd42e3ba311766a170764358ff9
eliassonepfl <fredrik.eliasson@epfl.ch>
2015-11-09 21:59:24 +0100

    more tests for navigation drawer

--- HikingApp/app/src/androidTest/java/ch/epfl/sweng/team7/hikingapp/HikeInfoActivityTest.java
+++ HikingApp/app/src/androidTest/java/ch/epfl/sweng/team7/hikingapp/HikeInfoActivityTest.java
@@ -10,11 +10,15 @@ import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.contrib.DrawerActions;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import org.junit.Before;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.contrib.DrawerActions.*;
 
@@ -46,6 +50,38 @@ public class HikeInfoActivityTest
         DrawerActions.closeDrawer(R.id.nav_drawer_layout);
 
     }
+
+    public void testClickItemInNavDrawer(){
+
+        DrawerActions.openDrawer(R.id.nav_drawer_layout);
+        onView(withId(R.id.nav_drawer)).perform(click());
+
+    }
+
+    public void testNavDrawerLoadItems(){
+
+        ListView navDrawerListView = (ListView) getActivity().findViewById(R.id.nav_drawer);
+        if(navDrawerListView.getAdapter().isEmpty()){
+            fail("ListView Empty");
+        }
+        else
+        {
+            if(!navDrawerListView.getAdapter().getItem(0).equals("Account")){
+                fail("First item should be Account");
+            }
+            if(!navDrawerListView.getAdapter().getItem(1).equals("Map")){
+                fail("Second item should be Map");
+            }
+            if(!navDrawerListView.getAdapter().getItem(2).equals("Hikes")){
+                fail("Third item shoud be Hikes");
+            }
+            if(!navDrawerListView.getAdapter().getItem(3).equals("Logout")){
+                fail("Fourth item should be Logout");
+            }
+        }
+    }
+
+    
 
 
 }
