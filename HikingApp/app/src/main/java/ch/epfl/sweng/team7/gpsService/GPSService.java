2bdb3e26eda7af432c965eb327a0c79cce749af8
Nuno Mota <hypernuno@hotmail.com>
2015-11-10 19:37:35 +0100

    Enables access to Service from other classes

--- HikingApp/app/src/main/java/ch/epfl/sweng/team7/gpsService/GPSService.java
+++ HikingApp/app/src/main/java/ch/epfl/sweng/team7/gpsService/GPSService.java
@@ -2,13 +2,26 @@ package ch.epfl.sweng.team7.gpsService;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
 public class GPSService extends Service {
+
+    private final IBinder mBinder = new LocalBinder();
+
+    /**
+     * Class for clients to access.
+     */
+    public class LocalBinder extends Binder {
+        GPSService getService() {
+            return GPSService.this;
+        }
+    }
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return mBinder;
     }
 }
