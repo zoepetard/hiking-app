93d8a11442258a67b9fa1737692a3f84568c88bd
Simon Schuetz <simon.schuetz@epfl.ch>
2015-11-13 22:45:51 +0100

    cleanup HikeListTest

--- HikingApp/app/src/androidTest/java/ch/epfl/sweng/team7/hikingapp/HikeListTest.java
+++ HikingApp/app/src/androidTest/java/ch/epfl/sweng/team7/hikingapp/HikeListTest.java
@@ -11,7 +11,6 @@ import org.junit.Test;
 import java.util.List;
 
 import ch.epfl.sweng.team7.database.DataManager;
-import ch.epfl.sweng.team7.database.DataManagerException;
 import ch.epfl.sweng.team7.database.HikeData;
 import ch.epfl.sweng.team7.mockServer.MockServer;
 import ch.epfl.sweng.team7.mockServer.MockServerTest;
@@ -24,28 +23,22 @@ public class HikeListTest {
             HikeListActivity.class);
 
     @Test
-    public void testCorrectData() {
+    public void testCorrectData() throws Exception {
         MockServer mockServer = new MockServer();
         MockServerTest mockServerTest = new MockServerTest();
         mockServerTest.mMockServer = mockServer;
         LatLngBounds bounds = new LatLngBounds(new LatLng(0.0, 0.0), new LatLng(5.0, 5.0));
-        try {
-            mockServerTest.setUp();
-            mockServerTest.testHikeCanBePosted();
-            mockServerTest.testHikeCanBePosted();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return;
-        }
+
+        mockServerTest.setUp();
+        mockServerTest.testHikeCanBePosted();
+        mockServerTest.testHikeCanBePosted();
+
         DataManager dataManager = DataManager.getInstance();
         dataManager.setDatabaseClient(mockServer);
         List<HikeData> hikeDatas = null;
-        try {
-            hikeDatas = dataManager.getHikesInWindow(bounds);
-        } catch (DataManagerException e) {
-            e.printStackTrace();
-            return;
-        }
+
+        hikeDatas = dataManager.getHikesInWindow(bounds);
+
         assertEquals(hikeDatas.get(0).getHikeId(), 1);
         assertEquals(hikeDatas.get(1).getHikeId(), 2);
         assertEquals(hikeDatas.get(0).getDate(), 123201);
