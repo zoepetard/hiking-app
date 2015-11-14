2ed0ee6432e5a52c4ce66e78679f03eedc2d7478
Ria Jiang <riarunjiejiang@gmail.com>
2015-11-14 19:49:25 +0100

    Edit UserDataActivity to display user data information

--- HikingApp/app/src/main/java/ch/epfl/sweng/team7/hikingapp/UserDataActivity.java
+++ HikingApp/app/src/main/java/ch/epfl/sweng/team7/hikingapp/UserDataActivity.java
@@ -2,6 +2,7 @@ package ch.epfl.sweng.team7.hikingapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.TextView;
 
 public class UserDataActivity extends Activity {
 
@@ -9,6 +10,16 @@ public class UserDataActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_data);
+
+        // add more field when we decide to store more user information
+        TextView user_name = (TextView) findViewById(R.id.user_name);
+        TextView user_email = (TextView) findViewById(R.id.user_email);
+        TextView num_hikes = (TextView) findViewById(R.id.num_hikes);
+
+        // use real data stored in local cache after issue #56 is in master
+        user_name.setText("Team 7");
+        user_email.setText("team7@epfl.ch");
+        num_hikes.setText(getString(R.string.num_hikes_fmt, Integer.toString(100)));
     }
 
 }
