1dae6d073ce9145ab724684d93c03bce4b5def5c
Ria Jiang <riarunjiejiang@gmail.com>
2015-11-14 19:39:14 +0100

    Add empty UserDataActivity and create its layout

---
+++ HikingApp/app/src/main/java/ch/epfl/sweng/team7/hikingapp/UserDataActivity.java
@@ -0,0 +1,14 @@
+package ch.epfl.sweng.team7.hikingapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class UserDataActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_data);
+    }
+
+}
