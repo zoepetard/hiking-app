ad1f430f406feffb5c79be1c14429dfa2dfa4fdf
Ria Jiang <riarunjiejiang@gmail.com>
2015-11-14 22:50:33 +0100

    Delete useless import in UserDataTest

--- HikingApp/app/src/androidTest/java/ch/epfl/sweng/team7/hikingapp/UserDataTest.java
+++ HikingApp/app/src/androidTest/java/ch/epfl/sweng/team7/hikingapp/UserDataTest.java
@@ -6,8 +6,6 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
