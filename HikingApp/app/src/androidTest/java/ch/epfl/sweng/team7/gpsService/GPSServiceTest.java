7f7d0aa645c5642629a08b24aee24b73b85f5582
Nuno Mota <hypernuno@hotmail.com>
2015-11-17 07:00:33 +0100

    Adds barebones of regression test

--- HikingApp/app/src/androidTest/java/ch/epfl/sweng/team7/gpsService/GPSServiceTest.java
+++ HikingApp/app/src/androidTest/java/ch/epfl/sweng/team7/gpsService/GPSServiceTest.java
@@ -1,4 +1,23 @@
 package ch.epfl.sweng.team7.gpsService;
 
-public class GPSServiceTest {
+import android.support.test.InstrumentationRegistry;
+import android.test.ActivityInstrumentationTestCase2;
+
+import ch.epfl.sweng.team7.hikingapp.MapActivity;
+
+public class GPSServiceTest extends ActivityInstrumentationTestCase2<MapActivity> {
+
+    public GPSServiceTest() {
+        super(MapActivity.class);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+    }
+
+    public void testServiceUnbindOnActivityChange() {
+        getActivity();
+    }
 }
