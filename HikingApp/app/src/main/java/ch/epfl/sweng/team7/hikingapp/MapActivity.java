38b58cba7d081c419067523c003b639ceb8933e2
Nuno Mota <hypernuno@hotmail.com>
2015-11-17 06:02:46 +0100

    Overrides onDestroy method in MapActivity

--- HikingApp/app/src/main/java/ch/epfl/sweng/team7/hikingapp/MapActivity.java
+++ HikingApp/app/src/main/java/ch/epfl/sweng/team7/hikingapp/MapActivity.java
@@ -56,6 +56,12 @@ public class MapActivity extends FragmentActivity {
         setUpMapIfNeeded();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        gps.unbindService(this);
+    }
+
     /**
      * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
      * installed) and the map has not already been instantiated.. This will ensure that we only ever
