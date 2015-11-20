cf069507efaa53f2d3edc7fb61b79351cf861b4c
Nuno Mota <hypernuno@hotmail.com>
2015-11-20 22:34:00 +0100

    Initializes NotificationHandler class as a Singleton

--- HikingApp/app/src/main/java/ch/epfl/sweng/team7/gpsService/NotificationHandler/NotificationHandler.java
+++ HikingApp/app/src/main/java/ch/epfl/sweng/team7/gpsService/NotificationHandler/NotificationHandler.java
@@ -1,4 +1,10 @@
 package ch.epfl.sweng.team7.gpsService.NotificationHandler;
 
 public class NotificationHandler {
+
+    private static NotificationHandler instance = new NotificationHandler();
+
+    public static NotificationHandler getInstance() {
+        return instance;
+    }
 }
