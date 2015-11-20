c8512955436d2bacbe941ef1dd52633e4a95f507
eliassonepfl <fredrik.eliasson@epfl.ch>
2015-11-20 21:20:01 +0100

    adds a singleton class for representing a signed in user

---
+++ HikingApp/app/src/main/java/ch/epfl/sweng/team7/hikingapp/SignedInUser.java
@@ -0,0 +1,43 @@
+package ch.epfl.sweng.team7.hikingapp;
+
+/**
+ * Creates a singleton representation of the user that is signed in
+ */
+public class SignedInUser {
+
+    private long mId;
+    private String mUserName;
+    private String mMailAddress;
+
+    private SignedInUser() {
+
+    }
+
+    private static class SignedInUserHolder {
+        private static final SignedInUser INSTANCE = new SignedInUser();
+    }
+
+    public SignedInUser getInstance() {
+        return SignedInUserHolder.INSTANCE;
+    }
+
+    public void init(long id, String userName, String mailAddress) {
+        mId = id;
+        mUserName = userName;
+        mMailAddress = mailAddress;
+    }
+
+    public long getId() {
+        return mId;
+    }
+
+    public String getUserName() {
+        return mUserName;
+    }
+
+    public String getMailAddress() {
+        return mMailAddress;
+    }
+
+
+}
