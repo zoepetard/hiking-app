931cf813b8882d20c6f9ccd47cd393a92d59c8f1
Nuno Mota <hypernuno@hotmail.com>
2015-11-20 22:51:56 +0100

    Adds method for Notification setup

--- HikingApp/app/src/main/java/ch/epfl/sweng/team7/gpsService/NotificationHandler/NotificationHandler.java
+++ HikingApp/app/src/main/java/ch/epfl/sweng/team7/gpsService/NotificationHandler/NotificationHandler.java
@@ -1,5 +1,15 @@
 package ch.epfl.sweng.team7.gpsService.NotificationHandler;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+import ch.epfl.sweng.team7.hikingapp.MapActivity;
+import ch.epfl.sweng.team7.hikingapp.R;
+
 public class NotificationHandler {
 
     private static NotificationHandler instance = new NotificationHandler();
@@ -8,5 +18,31 @@ public class NotificationHandler {
         return instance;
     }
 
-    private NotificationHandler() {}
+    public void setupFeedbackNotification(Context context) {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.drawable.notification_icon)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!");
+
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(context, MapActivity.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MapActivity.class);
+
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // mId allows you to update the notification later on.
+        mNotificationManager.notify(R.id.feedback_notification, mBuilder.build());
+    }
+
+    private NotificationHandler() {
+    }
 }
