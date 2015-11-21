3e712f9d017b19f371576600db455cb62672298f
Nuno Mota <hypernuno@hotmail.com>
2015-11-21 11:46:43 +0100

    Adds initial private fields and setter methods to class

--- HikingApp/app/src/main/java/ch/epfl/sweng/team7/hikingapp/mapActivityElements/BottomInfoView.java
+++ HikingApp/app/src/main/java/ch/epfl/sweng/team7/hikingapp/mapActivityElements/BottomInfoView.java
@@ -1,4 +1,22 @@
 package ch.epfl.sweng.team7.hikingapp.mapActivityElements;
 
+import android.widget.TextView;
+
 public class BottomInfoView {
+
+    private TextView title;
+    private TextView infoLine1;
+    private TextView infoLine2;
+
+    private void setTitle(String title) {
+
+    }
+
+    private void setInfoLine1(String infoMessage) {
+
+    }
+
+    private void setInfoLine2(String infoMessage) {
+        
+    }
 }
