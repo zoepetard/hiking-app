f319c5da013585c6e5ab4c1c1e221c15e1d00038
Nuno Mota <hypernuno@hotmail.com>
2015-11-21 15:09:22 +0100

    Makes use of toLocation method to avoid code duplication

--- HikingApp/app/src/main/java/ch/epfl/sweng/team7/gpsService/containers/GPSPath.java
+++ HikingApp/app/src/main/java/ch/epfl/sweng/team7/gpsService/containers/GPSPath.java
@@ -42,16 +42,8 @@ public final class GPSPath {
 
     public float distanceToStart() {
         try {
-            Location startLoc = new Location("");
-            startLoc.setLatitude(path.get(0).getGeoCoords().getLatitude());
-            startLoc.setLongitude(path.get(0).getGeoCoords().getLongitude());
-            startLoc.setAltitude(path.get(0).getGeoCoords().getAltitude());
-
-            Location lastLoc = new Location("");
-            lastLoc.setLatitude(path.get(path.size() - 1).getGeoCoords().getLatitude());
-            lastLoc.setLongitude(path.get(path.size() - 1).getGeoCoords().getLatitude());
-            lastLoc.setAltitude(path.get(path.size() - 1).getGeoCoords().getAltitude());
-
+            Location startLoc = path.get(0).toLocation();
+            Location lastLoc = path.get(path.size() - 1).toLocation();
             return (startLoc.distanceTo(lastLoc));
         } catch (IndexOutOfBoundsException e) {
             return 0f;
