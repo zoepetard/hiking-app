b0d175672a28b1624bd7bdd531234a32adb41630
Nuno Mota <hypernuno@hotmail.com>
2015-11-21 11:55:12 +0100

    Initializes private fields

--- HikingApp/app/src/main/java/ch/epfl/sweng/team7/hikingapp/mapActivityElements/BottomInfoView.java
+++ HikingApp/app/src/main/java/ch/epfl/sweng/team7/hikingapp/mapActivityElements/BottomInfoView.java
@@ -6,14 +6,17 @@ import android.widget.TextView;
 
 public class BottomInfoView {
 
-    private Context contex;
+    private Context context;
     private TableLayout mapTableLayout;
     private TextView title;
     private TextView infoLine1;
     private TextView infoLine2;
 
     public BottomInfoView(Context context) {
-
+        this.context = context;
+        this.title = new TextView(context);
+        this.infoLine1 = new TextView(context);
+        this.infoLine2 = new TextView(context);
     }
 
     private void setTitle(String title) {
