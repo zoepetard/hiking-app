20a9ebed8c88296a6a0b569e96620fb450c5d331
Nuno Mota <hypernuno@hotmail.com>
2015-11-21 13:48:10 +0100

    Changes elapsed time display/calculation for more familiar units

--- HikingApp/app/src/main/java/ch/epfl/sweng/team7/gpsService/GPSManager.java
+++ HikingApp/app/src/main/java/ch/epfl/sweng/team7/gpsService/GPSManager.java
@@ -125,7 +125,7 @@ public final class GPSManager {
             this.lastFootPrint = new GPSFootPrint(GeoCoords.fromLocation(newLocation), newLocation.getTime());
             if (this.mIsTracking) {
                 gpsPath.addFootPrint(this.lastFootPrint);
-                infoDisplay.setInfoLine(BOTTOM_TABLE_ACCESS_ID, 0, String.format("\t%d milli", gpsPath.timeElapsed()));
+                infoDisplay.setInfoLine(BOTTOM_TABLE_ACCESS_ID, 0, String.format("\t%d s", gpsPath.timeElapsedInSeconds()));
                 infoDisplay.setInfoLine(BOTTOM_TABLE_ACCESS_ID, 1, String.format("\t%d km travelled", gpsPath.distanceTraveled()));
             }
         }
