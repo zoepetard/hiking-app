9c7942c1a6972059c933c6fbc82da3c00da20a8c
Ria Jiang <riarunjiejiang@gmail.com>
2015-11-23 00:40:03 +0100

    Login go automatically to Map once logged in; delete useless buttons

--- HikingApp/app/src/main/java/ch/epfl/sweng/team7/hikingapp/LoginActivity.java
+++ HikingApp/app/src/main/java/ch/epfl/sweng/team7/hikingapp/LoginActivity.java
@@ -22,6 +22,9 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -29,6 +32,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -40,6 +44,8 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.plus.Plus;
 import com.google.android.gms.plus.model.people.Person;
+
+import java.io.InputStream;
 
 public class LoginActivity extends Activity implements
         View.OnClickListener,
@@ -65,10 +71,7 @@ public class LoginActivity extends Activity implements
     private static final String KEY_SHOULD_RESOLVE = "should_resolve";
 
     /* Client for accessing Google APIs */
-    private GoogleApiClient mGoogleApiClient;
-
-    /* View to display current status (signed-in, signed-out, disconnected, etc) */
-    private TextView mStatus;
+    private static GoogleApiClient mGoogleApiClient;
 
     // [START resolution_variables]
     /* Is there a ConnectionResult resolution in progress? */
@@ -93,19 +96,12 @@ public class LoginActivity extends Activity implements
 
         // Set up button click listeners
         findViewById(R.id.sign_in_button).setOnClickListener(this);
-        findViewById(R.id.sign_out_button).setOnClickListener(this);
-        findViewById(R.id.disconnect_button).setOnClickListener(this);
-        findViewById(R.id.change_name_button).setOnClickListener(this);
-        findViewById(R.id.goto_map_button).setOnClickListener(this);
 
         // Large sign-in
         ((SignInButton) findViewById(R.id.sign_in_button)).setSize(SignInButton.SIZE_WIDE);
 
         // Start with sign-in button disabled until sign-in either succeeds or fails
         findViewById(R.id.sign_in_button).setEnabled(false);
-
-        // Set up view instances
-        mStatus = (TextView) findViewById(R.id.status);
 
         // [START create_google_api_client]
         // Build GoogleApiClient with access to basic profile
@@ -125,34 +121,27 @@ public class LoginActivity extends Activity implements
             if (currentPerson != null) {
                 // Show signed-in user's name
                 String name = currentPerson.getDisplayName();
-                mStatus.setText(getString(R.string.signed_in_fmt, name));
 
                 // Show users' email address (which requires GET_ACCOUNTS permission)
                 if (checkAccountsPermission()) {
                     String currentAccount = Plus.AccountApi.getAccountName(mGoogleApiClient);
-                    ((TextView) findViewById(R.id.email)).setText(currentAccount);
                 }
+
+                String photoUrl = currentPerson.getImage().getUrl();
+//                new LoadProfileImage(profilePic).execute(photoUrl);
+
+                Intent i = new Intent(this, MapActivity.class);
+                startActivity(i);
             } else {
                 // If getCurrentPerson returns null there is generally some error with the
                 // configuration of the application (invalid Client ID, Plus API not enabled, etc).
                 Log.w(TAG, getString(R.string.error_null_person));
-                mStatus.setText(getString(R.string.signed_in_err));
             }
-
-            // Set button visibility
-            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
-            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
-            findViewById(R.id.change_display_name_and_goto_map).setVisibility(View.VISIBLE);
         } else {
             // Show signed-out message and clear email field
-            mStatus.setText(R.string.signed_out);
-            ((TextView) findViewById(R.id.email)).setText("");
 
             // Set button visibility
             findViewById(R.id.sign_in_button).setEnabled(true);
-            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
-            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
-            findViewById(R.id.change_display_name_and_goto_map).setVisibility(View.GONE);
         }
     }
 
@@ -338,18 +327,6 @@ public class LoginActivity extends Activity implements
             case R.id.sign_in_button:
                 onSignInClicked();
                 break;
-            case R.id.sign_out_button:
-                onSignOutClicked();
-                break;
-            case R.id.disconnect_button:
-                onDisconnectClicked();
-                break;
-            case R.id.change_name_button:
-                onChangeNameClicked();
-                break;
-            case R.id.goto_map_button:
-                onGotoMapClicked();
-                break;
         }
     }
     // [END on_click]
@@ -360,51 +337,42 @@ public class LoginActivity extends Activity implements
         // attempt to resolve any errors that occur.
         mShouldResolve = true;
         mGoogleApiClient.connect();
-
-        // Show a message to the user that we are signing in.
-        mStatus.setText(R.string.signing_in);
     }
     // [END on_sign_in_clicked]
 
     // [START on_sign_out_clicked]
-    private void onSignOutClicked() {
+    public static void onSignOutClicked() {
         // Clear the default account so that GoogleApiClient will not automatically
         // connect in the future.
         if (mGoogleApiClient.isConnected()) {
             Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
             mGoogleApiClient.disconnect();
         }
-
-        showSignedOutUI();
     }
     // [END on_sign_out_clicked]
 
-    // [START on_disconnect_clicked]
-    private void onDisconnectClicked() {
-        // Revoke all granted permissions and clear the default account.  The user will have
-        // to pass the consent screen to sign in again.
-        if (mGoogleApiClient.isConnected()) {
-            Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
-            Plus.AccountApi.revokeAccessAndDisconnect(mGoogleApiClient);
-            mGoogleApiClient.disconnect();
+    private class LoadProfileImage extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public LoadProfileImage(ImageView bmImage) {
+            this.bmImage = bmImage;
         }
 
-        showSignedOutUI();
-    }
-    // [END on_disconnect_clicked]
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
 
-    private void onChangeNameClicked() {
-        Intent i = new Intent(this, ChangeNicknameActivity.class);
-        startActivity(i);
-    }
-
-    private void onGotoMapClicked() {
-        Intent i = new Intent(this, MapActivity.class);
-        TextView textView = (TextView) findViewById(R.id.status);
-        String name = textView.getText().toString();
-        int colon = name.indexOf(":");
-        name = name.substring(colon + 2);
-        i.putExtra(EXTRA_NAME, name);
-        startActivity(i);
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
