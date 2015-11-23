6ab04f5e0ca6ea1e1a040ca262140d626c8b5688
Ria Jiang <riarunjiejiang@gmail.com>
2015-11-23 17:59:59 +0100

    Solve conflict

--- HikingApp/app/src/main/java/ch/epfl/sweng/team7/hikingapp/MapActivity.java
+++ HikingApp/app/src/main/java/ch/epfl/sweng/team7/hikingapp/MapActivity.java
@@ -75,7 +75,7 @@ public class MapActivity extends FragmentActivity {
         //Initializes the BottomInfoView
         createBottomInfoView();
 
-        createGoToHikesButton();
+        setGoToHikesButtonListener();
     }
 
     @Override
@@ -355,18 +355,8 @@ public class MapActivity extends FragmentActivity {
         return new LatLngBounds(guessSW, guessNE);
     }
 
-    private void createGoToHikesButton() {
-        Button goHikeButton = new Button(this);
-        goHikeButton.setText(R.string.go_hikes);
-
-        RelativeLayout layout = (RelativeLayout) findViewById(R.id.mapLayout);
-        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        lp.addRule(RelativeLayout.ALIGN_PARENT_TOP);
-        lp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-
-        goHikeButton.setLayoutParams(lp);
-        layout.addView(goHikeButton, lp);
-
+    private void setGoToHikesButtonListener() {
+        Button goHikeButton = (Button) findViewById(R.id.go_hikes_button);
         goHikeButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 LatLngBounds bounds = getBounds();
