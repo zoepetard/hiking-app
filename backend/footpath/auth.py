9decde1dfc2cf885cc595b9a29e37573f97f5f78
Simon Schuetz <simon.schuetz@epfl.ch>
2015-11-28 19:01:50 +0100

    disables permissions for backwards compatibility

--- backend/footpath/auth.py
+++ backend/footpath/auth.py
@@ -1,30 +1,35 @@
 from google.appengine.ext import ndb
 from footpath.user import *
 
+AUTH_FORBIDDEN = -1
+
 def authenticate(request):
     auth_header = request.META.get('HTTP_AUTH_HEADER', '')
     
-    # Temporary: Backwards compatibility TODO(simon) remove
     if len(auth_header) == 0:
-        return int(request.META.get('HTTP_AUTH_USER_ID', 0))
+        return AUTH_FORBIDDEN
 
     auth_header = json.loads(auth_header)
     user_id = auth_header['user_id']
     mail_address = auth_header['mail_address']
     token = auth_header['token']
     if user_id <= 0:
-        return -1
+        return AUTH_FORBIDDEN
 
     user = ndb.Key(User, user_id).get()
     if not user:
-        return -1
+        return AUTH_FORBIDDEN
     if (user.mail_address != mail_address) or (user.db_token != token):
-        return -1
+        return AUTH_FORBIDDEN
 
     return user_id
 
 def has_query_permission(visitor_id):
+    # Temporary: Backwards compatibility TODO(simon) remove
+    return True
     return visitor_id > 0
 
 def has_write_permission(visitor_id, owner_id):
+    # Temporary: Backwards compatibility TODO(simon) remove
+    return True
     return visitor_id == owner_id
