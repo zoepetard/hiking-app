0332da22dd8d1d3c1aa444b82c72c33f99b9815b
Simon Schuetz <simon.schuetz@epfl.ch>
2015-11-29 12:31:34 +0100

    implements JSON parser for rating

--- HikingApp/app/src/main/java/ch/epfl/sweng/team7/database/Rating.java
+++ HikingApp/app/src/main/java/ch/epfl/sweng/team7/database/Rating.java
@@ -1,17 +1,29 @@
 package ch.epfl.sweng.team7.database;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by simon on 11/29/15.
  */
 public class Rating {
     private final double mAverage;
     private final int mCount;
-    private final int mUserRating;
+    private final Integer mUserRating;
 
     public Rating(double average, int count, int userRating) {
         mAverage = average;
         mCount = count;
-        mUserRating = userRating;
+        // A negative rating denotes "not voted yet"
+        if(userRating >= 0) {
+            mUserRating = new Integer(userRating);
+        } else {
+            mUserRating = null;
+        }
+    }
+
+    public boolean userHasVoted() {
+        return (mUserRating != null);
     }
 
     /**
@@ -25,10 +37,14 @@ public class Rating {
         }
     }
 
-    /**
-     * Return whether the user has already voted
-     */
-    public boolean userHasVoted() {
-        return mUserRating >= 0;
+    public int getVoteCount() {
+        return mCount;
+    }
+
+    public static Rating parseFromJSON(JSONObject jsonObject) throws JSONException {
+        return new Rating(
+                jsonObject.getDouble("rating"),
+                jsonObject.getInt("count"),
+                jsonObject.getInt("visitor_rating"));
     }
 }
