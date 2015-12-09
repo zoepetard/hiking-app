33f5a808630f8a71f7386c8689e81b029b88512f
Nuno Mota <hypernuno@hotmail.com>
2015-12-09 17:03:04 +0100

    Creates special Tuple class

--- HikingApp/app/src/main/java/ch/epfl/sweng/team7/hikingapp/MapActivity.java
+++ HikingApp/app/src/main/java/ch/epfl/sweng/team7/hikingapp/MapActivity.java
@@ -448,6 +448,21 @@ public class MapActivity extends FragmentActivity {
         }
     }
 
+    private class Tuple<X, Y> {
+        private final X mMember1;
+        private final Y mMember2;
+        public Tuple(X x, Y y) {
+            mMember1 = x;
+            mMember2 = y;
+        }
+        public X getmMember1() {
+            return mMember1;
+        }
+        public Y getmMember2() {
+            return mMember2;
+        }
+    }
+
     private class DisplayHikeInfo extends AsyncTask<HikeData, Void, UserData> {
 
         HikeData hike = null;
