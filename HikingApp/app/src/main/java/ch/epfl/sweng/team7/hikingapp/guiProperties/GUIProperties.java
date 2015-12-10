06dc19ad2504b1146cf6d58f6a16ad8fb883ecc2
Nuno Mota <hypernuno@hotmail.com>
2015-12-10 16:25:04 +0100

    Adds constants to class

--- HikingApp/app/src/main/java/ch/epfl/sweng/team7/hikingapp/guiProperties/GUIProperties.java
+++ HikingApp/app/src/main/java/ch/epfl/sweng/team7/hikingapp/guiProperties/GUIProperties.java
@@ -1,4 +1,6 @@
 package ch.epfl.sweng.team7.hikingapp.guiProperties;
 
 public final class GUIProperties {
+    public static final int DEFAULT_BUTTON_SIZE = 64;
+    public static final int DEFAULT_BUTTON_MARGIN = 10;
 }
