fb7a643b40d569a8ffe3efec73ddc6544683a616
Nuno Mota <hypernuno@hotmail.com>
2015-12-10 18:25:24 +0100

    Adds test for initialization equality

--- HikingApp/app/src/androidTest/java/ch/epfl/sweng/team7/gpsService/containers/GPSFootPrintTest.java
+++ HikingApp/app/src/androidTest/java/ch/epfl/sweng/team7/gpsService/containers/GPSFootPrintTest.java
@@ -5,11 +5,22 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import ch.epfl.sweng.team7.gpsService.containers.coordinates.GeoCoords;
+
+import static org.junit.Assert.assertEquals;
+
 @RunWith(AndroidJUnit4.class)
 public class GPSFootPrintTest {
 
     @Test(expected = NullPointerException.class)
     public void testInitializationWithNullGeoCoords() {
         new GPSFootPrint(null, 0);
+    }
+
+    @Test
+    public void testInitializationEquality() {
+        GeoCoords newCoords = new GeoCoords(10, 0, 0);
+        GPSFootPrint footPrint = new GPSFootPrint(newCoords, 0);
+        assertEquals(footPrint.getGeoCoords(), newCoords);
     }
 }
