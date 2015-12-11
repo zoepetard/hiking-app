cbdfef5c6732e7fc511085749ce67597403d5f01
Nuno Mota <hypernuno@hotmail.com>
2015-12-11 02:44:37 +0100

    Fixes generic method

--- HikingApp/app/src/main/java/ch/epfl/sweng/team7/hikingapp/guiProperties/GUIProperties.java
+++ HikingApp/app/src/main/java/ch/epfl/sweng/team7/hikingapp/guiProperties/GUIProperties.java
@@ -1,21 +1,30 @@
 package ch.epfl.sweng.team7.hikingapp.guiProperties;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 public final class GUIProperties {
     public static final int DEFAULT_BUTTON_SIZE = 64;
     public static final int DEFAULT_BUTTON_MARGIN = 10;
 
-    public static void setupButton(Context context, int buttonId, int backgroundResource) {
+    public static void setupButton(Context context, int buttonId, int backgroundResource, String layoutType) {
         Button backButton = (Button) ((Activity)context).findViewById(buttonId);
         backButton.setText("");
         backButton.setBackgroundResource(backgroundResource);
-        RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) backButton.getLayoutParams();
-        lp.width = DEFAULT_BUTTON_SIZE;
-        lp.height = DEFAULT_BUTTON_SIZE;
-        lp.setMargins(DEFAULT_BUTTON_MARGIN, DEFAULT_BUTTON_MARGIN, DEFAULT_BUTTON_MARGIN, DEFAULT_BUTTON_MARGIN);
+        if (layoutType == "Relative") {
+            RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) backButton.getLayoutParams();
+            lp.width = DEFAULT_BUTTON_SIZE;
+            lp.height = DEFAULT_BUTTON_SIZE;
+            lp.setMargins(DEFAULT_BUTTON_MARGIN, DEFAULT_BUTTON_MARGIN, DEFAULT_BUTTON_MARGIN, DEFAULT_BUTTON_MARGIN);
+        } else {
+            LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) backButton.getLayoutParams();
+            lp.width = DEFAULT_BUTTON_SIZE;
+            lp.height = DEFAULT_BUTTON_SIZE;
+            lp.setMargins(DEFAULT_BUTTON_MARGIN, DEFAULT_BUTTON_MARGIN, DEFAULT_BUTTON_MARGIN, DEFAULT_BUTTON_MARGIN);
+        }
     }
 }
